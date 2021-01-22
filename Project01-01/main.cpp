a59197c08df59c2c99cf0f65120207f8de7ebdd2
James Fleming <jflemi9@uic.edu>
2021-01-22 01:13:48 -0600

    [update] Finished part 2 of project 1.

--- Project01-01/main.cpp
+++ Project01-01/main.cpp
@@ -47,6 +47,135 @@ int main() {
     cout << "Account " << acct4 << ": balance $" << bal4 << endl;
     cout << "Account " << acct5 << ": balance $" << bal5 << endl;
 
+    int flag = 1;
+    cout << "** Processing user commands..." << endl;
+
+    while (flag == 1) {
+        cout << "Enter command (+, -, ?, ^, *, x):" << endl;
+        string command;
+        cin >> command;
+
+        // deposit
+        if (command == "+") {
+            int account;
+            double balance;
+            cin >> account;
+            cin >> balance;
+
+            if (account == acct1) {
+                bal1 += balance;
+                cout << "Account " << acct1 << ": balance $" << bal1 << endl;
+            } else if (account == acct2) {
+                bal2 += balance;
+                cout << "Account " << acct2 << ": balance $" << bal2 << endl;
+            } else if (account == acct3) {
+                bal3 += balance;
+                cout << "Account " << acct3 << ": balance $" << bal3 << endl;
+            } else if (account == acct4) {
+                bal4 += balance;
+                cout << "Account " << acct4 << ": balance $" << bal4 << endl;
+            } else if (account == acct5) {
+                bal5 += balance;
+                cout << "Account " << acct5 << ": balance $" << bal5 << endl;
+            } else {
+                cout << "** Invalid account, transaction ignored" << endl;
+            }
+            ofstream outfile;
+            outfile.open(infilename);
+            outfile << acct1 << " " << bal1 << endl;
+            outfile << acct2 << " " << bal2 << endl;
+            outfile << acct3 << " " << bal3 << endl;
+            outfile << acct4 << " " << bal4 << endl;
+            outfile << acct5 << " " << bal5 << endl;
+            outfile.close();
+        // withdrawl
+        } else if (command == "-") {
+            int account;
+            double balance;
+            cin >> account;
+            cin >> balance;
+
+            if (account == acct1) {
+                bal1 -= balance;
+                cout << "Account " << acct1 << ": balance $" << bal1 << endl;
+            } else if (account == acct2) {
+                bal2 -= balance;
+                cout << "Account " << acct2 << ": balance $" << bal2 << endl;
+            } else if (account == acct3) {
+                bal3 -= balance;
+                cout << "Account " << acct3 << ": balance $" << bal3 << endl;
+            } else if (account == acct4) {
+                bal4 -= balance;
+                cout << "Account " << acct4 << ": balance $" << bal4 << endl;
+            } else if (account == acct5) {
+                bal5 -= balance;
+                cout << "Account " << acct5 << ": balance $" << bal5 << endl;
+            } else {
+                cout << "** Invalid account, transaction ignored" << endl;
+            }
+
+            ofstream outfile;
+            outfile.open(infilename);
+            outfile << acct1 << " " << bal1 << endl;
+            outfile << acct2 << " " << bal2 << endl;
+            outfile << acct3 << " " << bal3 << endl;
+            outfile << acct4 << " " << bal4 << endl;
+            outfile << acct5 << " " << bal5 << endl;
+            outfile.close();
+        // check balance
+        } else if (command == "?") {
+            int account;
+            cin >> account;
+            if (account == acct1) {
+                cout << "Account " << acct1 << ": balance $" << bal1 << endl;
+            } else if (account == acct2) {
+                cout << "Account " << acct2 << ": balance $" << bal2 << endl;
+            } else if (account == acct3) {
+                cout << "Account " << acct3 << ": balance $" << bal3 << endl;
+            } else if (account == acct4) {
+                cout << "Account " << acct4 << ": balance $" << bal4 << endl;
+            } else if (account == acct5) {
+                cout << "Account " << acct5 << ": balance $" << bal5 << endl;
+            } else {
+                cout << "** Invalid account, transaction ignored" << endl;
+            }
+        // see highest account
+        } else if (command == "^") {
+            double largest = bal1;
+            largest = largest > bal2 ? largest : bal2;
+            largest = largest > bal3 ? largest : bal3;
+            largest = largest > bal4 ? largest : bal4;
+            largest = largest > bal5 ? largest : bal5;
+
+            if (largest == bal1) {
+                cout << "Account " << acct1 << ": balance $" << bal1 << endl;
+            } else if (largest == bal2) {
+                cout << "Account " << acct2 << ": balance $" << bal2 << endl;
+            } else if (largest == bal3) {
+                cout << "Account " << acct3 << ": balance $" << bal3 << endl;
+            } else if (largest == bal4) {
+                cout << "Account " << acct4 << ": balance $" << bal4 << endl;
+            } else if (largest == bal5) {
+                cout << "Account " << acct5 << ": balance $" << bal5 << endl;
+            }
+        // list all accounts
+        } else if (command == "*") {
+            cout << "Account " << acct1 << ": balance $" << bal1 << endl;
+            cout << "Account " << acct2 << ": balance $" << bal2 << endl;
+            cout << "Account " << acct3 << ": balance $" << bal3 << endl;
+            cout << "Account " << acct4 << ": balance $" << bal4 << endl;
+            cout << "Account " << acct5 << ": balance $" << bal5 << endl;
+        // exit
+        } else if (command == "x") {   
+            flag = 0;
+        } else {
+            cout << "** Invalid command, try again..." << endl;
+        }
+    }
+    
+    /*
+    not sure if this is needed for part two of the project??
+
     // +1 dollar
     infile.close();
     cout << "** Adding $1 to each balance..." << endl;
@@ -72,6 +201,7 @@ int main() {
     outfile << acct5 << " " << bal5 << endl;
 
     outfile.close();
+    */
 
     cout << "** Saving account data..." << endl;
     cout << "** Done ** " << endl;
